46f62a33040c53b7978e4ac10d6ba3091f463136
jenmarieng <jermaineng04@gmail.com>
2025-01-26 13:34:20 +0800

    Add max loan amount

--- src/main/java/Loan.java
+++ src/main/java/Loan.java
@@ -4,11 +4,17 @@ public class Loan {
     private boolean isPaid;
     private double interestRate;
 
+    public static int maxLoanAmount = 10000;
+
     public Loan(String borrower, int amount, double interestRate) {
         this.borrower = borrower;
         this.amount = amount;
         this.interestRate = interestRate;
         this.isPaid = false;
+
+        if (amount > maxLoanAmount) {
+            amount = maxLoanAmount;
+        }
     }
 
     public void setAsPaid() {
